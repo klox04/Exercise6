a5e422951fb1f23db659f4b0b01b6039b662b3d5
Daniel Caralos <109465862+klox04@users.noreply.github.com>
2023-04-13 11:00:23 +0800

    second commit

--- prac.py
+++ prac.py
@@ -1,12 +1,14 @@
-print ("DanielCaralos")
+NUM1 =0
+NUM2 =0
+ans = 0
+
+NUM1 = int(input("Enter 1st Number\n"))
+
+
+NUM2 = int(input("Enter 2nd Nnmber\n"))
+ans = NUM1 + NUM2
+
+print(ans)
 
 
 
-let = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m"
-,"n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-let1 = [" ","z","y","x","w","v","u","t","s","r","p","q","o","n"
-,"m","l","k","j","i","h","g","f","e","d","c","b","a"]
-
-lett = input ("Input a word: ")
-ans=""
